41c1c3b1718860f593600ff409c28ee3e009ac06
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-11-30 09:44:10 +0800

    some stuff

--- app/utilities/Navbar.tsx
+++ app/utilities/Navbar.tsx
@@ -3,28 +3,34 @@
 import Link from "next/link";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  NavigationMenuContent
 } from "@/components/ui/navigation-menu";
 import ThemeSwitch from "../ThemeSwitch";
-import Image from "next/image";
-import logo from '.././open-motion-high-resolution-logo-transparent.png'
+//import Image from "next/image";
 export default function Navbar() {
   return (
-    <div className="sticky top-2 z-20 mx-auto p-3 max-w-fit rounded-lg backdrop-blur-md dark:bg-black">
-        <NavigationMenu>
-          <NavigationMenuList className="flex items-center gap-4">
-            <NavigationMenuItem>
-                <Link href="/"><Image className="rounded-3xl" src={logo} alt="OpenMotion" width={110} height={50}/></Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <ThemeSwitch />
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+    <div className="sticky top-1.5 z-20 mx-auto p-3 max-w-fit rounded-lg backdrop-blur-md dark:bg-black">
+      <NavigationMenu>
+        <NavigationMenuList className="flex items-center gap-4">
+          <NavigationMenuItem>
+            <Link href="/" className="pr-16">Logo</Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex flex-col text-center align-text-top">
+            <NavigationMenuTrigger>
+              Products
+              <NavigationMenuContent className="p-2">
+                Test...
+              </NavigationMenuContent>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <ThemeSwitch />
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 }
